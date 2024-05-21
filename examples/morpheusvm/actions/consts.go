1c819aee291d9db5d0fb33d0a352121cc9dbde83
Aliasgar Merchant <amercha1@stevens.edu>
2024-05-21 21:40:24 +0200

    Add Mint to registry

--- examples/morpheusvm/actions/consts.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package actions
-
-const TransferComputeUnits = 1
-const MintComputeUnits = 1
